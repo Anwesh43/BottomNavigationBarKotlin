9327c9b8562f1e1a974487ef642c6bfbec32c863
Anwesh43 <anweshthecool0@gmail.com>
2017-08-11 17:44:03 +0530

    adding BottomNavigationBar

--- bottomnavigationbarkotlin/src/main/java/com/anwesome/ui/bottomnavigationbarkotlin/BottomNavigationBar.kt
+++ bottomnavigationbarkotlin/src/main/java/com/anwesome/ui/bottomnavigationbarkotlin/BottomNavigationBar.kt
@@ -1,15 +1,67 @@
 package com.anwesome.ui.bottomnavigationbarkotlin
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Point
+import android.hardware.display.DisplayManager
 import android.view.ViewGroup
+import android.widget.HorizontalScrollView
 
 /**
  * Created by anweshmishra on 11/08/17.
  */
-class BottomNavigationBar {
-    companion object {
-        fun create(activity: Activity) {
+class BottomNavigationBar(ctx:Context):ViewGroup(ctx) {
+    var w:Int = 0
+    var h:Int = 0
+    var hSize:Int = 0
+    init {
+        initWH(ctx)
+    }
+    private fun initWH(ctx:Context) {
+        var displayManager = ctx.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        var display = displayManager.getDisplay(0)
+        var size = Point()
+        display.getRealSize(size)
+        w = size.x
+        h = size.y
+        hSize = h/9
+    }
+    override fun onMeasure(wspec:Int,hspec:Int) {
+        var wView = 0
+        for(i in 0..childCount-1) {
+            var child = getChildAt(i)
+            measureChild(child,wspec,hspec)
+            wView += child.measuredWidth + w/12
+        }
+        setMeasuredDimension(wView,hSize)
+    }
+    override fun onLayout(change:Boolean,a:Int,b:Int,wv:Int,hv:Int) {
+        var x = 0
+        for(i in 0..childCount-1) {
+            var child = getChildAt(i)
+            child.layout(x,h/30,x+child.measuredWidth,h/30+child.measuredHeight)
+            x += child.measuredWidth + w/12
+        }
 
+    }
+    fun addButton() {
+
+    }
+    companion object {
+        var viewCreated = false
+        var bar:BottomNavigationBar?=null
+        fun create(activity: Activity) {
+            if(!viewCreated) {
+                bar = BottomNavigationBar(activity)
+                viewCreated = true
+            }
+        }
+        fun show(activity: Activity) {
+            if(viewCreated) {
+                var scrollView = HorizontalScrollView(activity)
+                scrollView.addView(bar, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
+                activity.addContentView(scrollView, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT))
+            }
         }
     }
 }
