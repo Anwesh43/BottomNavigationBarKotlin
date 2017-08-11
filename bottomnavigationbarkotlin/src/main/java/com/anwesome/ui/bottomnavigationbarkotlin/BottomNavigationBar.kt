04c7152be4c788efc4fb65e02f17e1232325a168
Anwesh43 <anweshthecool0@gmail.com>
2017-08-11 17:19:55 +0530

    adding bottomnavigationbar module

---
+++ bottomnavigationbarkotlin/src/main/java/com/anwesome/ui/bottomnavigationbarkotlin/BottomNavigationBar.kt
@@ -0,0 +1,15 @@
+package com.anwesome.ui.bottomnavigationbarkotlin
+
+import android.app.Activity
+import android.view.ViewGroup
+
+/**
+ * Created by anweshmishra on 11/08/17.
+ */
+class BottomNavigationBar {
+    companion object {
+        fun create(activity: Activity) {
+
+        }
+    }
+}
