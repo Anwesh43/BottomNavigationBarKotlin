9586962063da300ffa56b2ac7dfa630ab4a42aba
Anwesh43 <anweshthecool0@gmail.com>
2017-08-11 19:17:25 +0530

    using BottomNavigationBar in activity

--- app/src/main/java/com/anwesome/ui/bottomnavigationbarkotlindemo/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/bottomnavigationbarkotlindemo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.anwesome.ui.bottomnavigationbarkotlindemo
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesome.ui.bottomnavigationbarkotlin.BottomNavigationBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var bitmap = BitmapFactory.decodeResource(resources,R.drawable.js)
+        BottomNavigationBar.create(this)
+        for(i in 0..5) {
+            BottomNavigationBar.addButton(bitmap)
+        }
+        BottomNavigationBar.show(this)
     }
 }
